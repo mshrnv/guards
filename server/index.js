2b9cdee138d7c2ab9c6dcd5b7a3b28d815fdf32e
shmd001 <cool.shoronov19@gmail.com>
2023-04-11 00:14:31 +0300

    Models defined

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./database')
+const models = require('./models/models')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
 
 const start = async () => {
     try {
---
+++ server/models/models.js
@@ -0,0 +1,54 @@
+const sequelize = require('../database')
+const {DataTypes} = require('sequelize')
+
+const Student = sequelize.define('student', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    fio: {type: DataTypes.STRING, unique: true, allowNull: false},
+    group: {type: DataTypes.INTEGER, allowNull: false},
+    isActive: {type: DataTypes.BOOLEAN, defaultValue: true},
+})
+
+const Guard = sequelize.define('guard', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    date: {type: DataTypes.DATEONLY, allowNull: false},
+})
+
+const Type = sequelize.define('type', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, allowNull: false, unique: true},
+})
+
+const Role = sequelize.define('role', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, allowNull: false, unique: true},
+})
+
+const TypeRole = sequelize.define('type_role', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    count: {type: DataTypes.INTEGER, allowNull: false},
+})
+
+const StudentGuard = sequelize.define('student_guard', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+Type.hasMany(Guard)
+Guard.belongsTo(Type)
+
+Role.hasMany(StudentGuard)
+StudentGuard.belongsTo(Role)
+
+Student.belongsToMany(Guard, {through: StudentGuard})
+Guard.belongsToMany(Student, {through: StudentGuard})
+
+Type.belongsToMany(Role, {through: TypeRole})
+Role.belongsToMany(Type, {through: TypeRole})
+
+module.exports = {
+    Student,
+    Guard,
+    StudentGuard,
+    Role,
+    Type,
+    TypeRole
+}
